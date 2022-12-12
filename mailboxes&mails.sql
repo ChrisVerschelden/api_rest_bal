5fd9ecb3731d23e474040e1ec24130ab38a6fefb
ChrisVerschelden <71574021+ChrisVerschelden@users.noreply.github.com>
2022-12-12 09:59:02 +0100

    ajout get emetteurs (#2)

--- mailboxes&mails.sql
+++ mailboxes&mails.sql
@@ -24,31 +24,34 @@ INSERT INTO `mailboxes` (`nom`, `emetteur_id`) VALUES
 
 ALTER TABLE `mailboxes`
   ADD UNIQUE KEY `nom` (`nom`);
+ALTER TABLE `mailboxes`
   ADD KEY (`emetteur_id`);
 
 ##### mails
 
 CREATE TABLE `mails` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `read` BOOLEAN NOT NULL,
+  `read` TINYINT NOT NULL,
   `title` varchar(255) CHARACTER SET utf8mb3 NOT NULL,
   `message` LONGTEXT CHARACTER SET utf8mb3 NOT NULL,
   `emetteur_id` int(11) NOT NULL,
   `mailboxe_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `mails` VALUES (`read`, `title`, `message`, `emetteur_id`, `mailboxe_id`)
-  (0, `perpignan`, `c'est une magnifique ville (non.)`, 1, 1);
+INSERT INTO `mails` (`read`, `title`, `message`, `emetteur_id`, `mailboxe_id`) VALUES 
+  (false, "perpignan", "c'est une magnifique ville (non.)", 1, 1);
 
 ALTER TABLE `mails`
   ADD KEY (`emetteur_id`);
+ALTER TABLE `mails`
   ADD KEY (`mailboxe_id`);
 
 #### ajout constraints
 
 ALTER TABLE `mails`
   ADD CONSTRAINT `mails_ibfk_1` FOREIGN KEY (`mailboxe_id`) REFERENCES `mailboxes` (`id`);
+ALTER TABLE `mails`
   ADD CONSTRAINT `mails_ibfk_2` FOREIGN KEY (`emetteur_id`) REFERENCES `emetteurs` (`id`);
 
 ALTER TABLE `mailboxes`
-  ADD CONSTRAINT `mails_ibfk_1` FOREIGN KEY (`emetteur_id`) REFERENCES `emetteurs` (`id`);
+  ADD CONSTRAINT `mailboxes_ibfk_1` FOREIGN KEY (`emetteur_id`) REFERENCES `emetteurs` (`id`);
